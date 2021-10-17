9e46d2aaf091f564c9d42a60bc10d804bee91c64
Riobener <venisonsheff@gmail.com>
2021-10-17 23:12:01 +0300

    [Test] BpmValidation

---
+++ practice3/src/com/riobener/UserInputValidation.java
@@ -0,0 +1,12 @@
+package com.riobener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class UserInputValidation {
+    public int validateBPM(String count) {
+        return 0;
+    }
+}
+
