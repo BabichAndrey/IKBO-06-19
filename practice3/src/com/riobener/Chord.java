35c0b6f169cec050014230ff8457e2e92f1d1cbc
Andrey <andreybabich2012@yandex.ru>
2021-10-17 23:43:18 +0300

    [Impl]ChordClass

--- practice3/src/com/riobener/Chord.java
+++ practice3/src/com/riobener/Chord.java
@@ -8,11 +8,15 @@ public class Chord {
     private final List<Integer> chord= new ArrayList<>();
 
     public List<Integer> getMajorChord(Note note) {
-
+        chord.add(note.getMidiNote());
+        chord.add(note.getMidiNote() + 4);
+        chord.add(note.getMidiNote()+ 7);
         return chord;
     }
     public List<Integer> getMinorChord(Note note) {
-
+        chord.add(note.getMidiNote());
+        chord.add(note.getMidiNote() + 3);
+        chord.add(note.getMidiNote()+ 7);
         return chord;
     }
 }
